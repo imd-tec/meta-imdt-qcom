d8a7dbe45bc1d4c32a3b8f78df4256db3954ddeb
dimkaps <150019158+dimkaps@users.noreply.github.com>
2026-02-16 12:30:54 +0000

    Enable DMIC4 for Microphone (#36)
    -Modify DeviceTX graphs to get sources from CODEC_DMA-LPAIF_RXTX-TX-3
    -Modify mixer commands to start DMIC4 when pulsesrc is starting.
    
    pulsesrc starts automatically the amic5, but it needs to activate the routing for DMIC4.
    Updated qtitinymix routes for DMIC4 on the startup stream as well.

--- recipes-audio/audio-init-service/audio-init-service/setup-audio.sh
+++ recipes-audio/audio-init-service/audio-init-service/setup-audio.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# Enable MI2S DAC for Playback 
 qtitinymix "Left DAC Mux" DAC_L2
 qtitinymix "Right DAC Mux" DAC_R2
 qtitinymix "HP Playback Volume" 9 9
+
+# Enable DMIC4 for Record 
+qtitinymix 'TX DEC0 MUX' 'MSM_DMIC'
+qtitinymix 'TX DMIC MUX0' 'DMIC4'
+qtitinymix 'TX_AIF1_CAP Mixer DEC0' 1
+qtitinymix 'TX_DEC0 Volume' 84
